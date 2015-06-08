AICP/frameworks_base

Action: Add kill-app and last-app actions
Forward port from KitKat

Change-Id: If8326ffed21faa2452e22b2afb717f4c123bd2a0

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -60,6 +60,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_SCREEN_PIN_REQUEST            = 18 << MSG_SHIFT;
     private static final int MSG_SET_AUTOROTATE_STATUS              = 19 << MSG_SHIFT;
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 20 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP                    = 21 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP                    = 22 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -105,6 +107,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void showScreenPinningRequest();
         public void setAutoRotate(boolean enabled);
         public void showCustomIntentAfterKeyguard(Intent intent);
+        public void toggleLastApp();
+        public void toggleKillApp();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -277,6 +281,20 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void toggleLastApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_LAST_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_LAST_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
+    public void toggleKillApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_KILL_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_KILL_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             if (mPaused) {
@@ -369,6 +387,12 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD:
                     mCallbacks.showCustomIntentAfterKeyguard((Intent) msg.obj);
                     break;
+               case MSG_TOGGLE_LAST_APP:
+                    mCallbacks.toggleLastApp();
+                    break;
+                case MSG_TOGGLE_KILL_APP:
+                    mCallbacks.toggleKillApp();
+                    break;	
             }
         }
     }

rr-cancro/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -61,6 +61,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_HIDE_HEADS_UP                      = 19 << MSG_SHIFT;
     private static final int MSG_SHOW_SCREEN_PIN_REQUEST            = 20 << MSG_SHIFT;
     private static final int MSG_SET_PIE_TRIGGER_MASK               = 21 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP                    = 22 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP                    = 23 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -107,7 +109,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void scheduleHeadsUpClose();
         public void showScreenPinningRequest();
         public void setPieTriggerMask(int newMask, boolean lock);
-
+        public void toggleLastApp();
+        public void toggleKillApp();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -265,6 +268,21 @@ public class CommandQueue extends IStatusBar.Stub {
             m.sendToTarget();
         }
     }
+
+
+    public void toggleLastApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_LAST_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_LAST_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
+    public void toggleKillApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_KILL_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_KILL_APP, 0, 0, null).sendToTarget();
+        }
+    }
     
     public void scheduleHeadsUpClose() {
         synchronized (mList) {
@@ -382,6 +400,12 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_SET_PIE_TRIGGER_MASK:
                     mCallbacks.setPieTriggerMask(msg.arg1, msg.arg2 != 0);
                     break;
+               case MSG_TOGGLE_LAST_APP:
+                    mCallbacks.toggleLastApp();
+                    break;
+                case MSG_TOGGLE_KILL_APP:
+                    mCallbacks.toggleKillApp();
+                    break;	
             }
         }
     }